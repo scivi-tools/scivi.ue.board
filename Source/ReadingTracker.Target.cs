3ed87b34d32390f8498e32afdfec7cf2cbe14da2
unknown <goldnotch@mail.ru>
2022-08-31 12:58:53 +0500

    add feature to pack project, but Engine compiled from source is required

--- Source/ReadingTracker.Target.cs
+++ Source/ReadingTracker.Target.cs
@@ -8,8 +8,9 @@ public class ReadingTrackerTarget : TargetRules
 	public ReadingTrackerTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
+		//BuildEnvironment = TargetBuildEnvironment.Unique;// to resolve editing bForceEnableExceptions
+		//bForceEnableExceptions = true;// enable exceptions for packaging
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-
 		ExtraModuleNames.AddRange( new string[] { "ReadingTracker" } );
 	}
 }
